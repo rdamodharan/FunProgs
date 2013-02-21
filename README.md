4e513629c1b79d3f16fca4b1fa73b8a590f2e472
Damodharan Rajalingam <damu.devnull@gmail.com>
2013-02-22 00:15:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Code for Fun
+
+This project contains small programs which I wrote in various programming
+languages for fun and learning
---
+++ stem_leaf/stem_leaf.erl
@@ -0,0 +1,35 @@
+-module(stem_leaf).
+-export([plot/1]).
+
+bucket(N) when N < -9 -> 
+    N - ((N+10) rem 10);
+bucket(N) ->
+    (N+10) - ((N+10) rem 10).
+
+leaf(N, Bucket) when N < 0 ->
+    Bucket - N;
+leaf(N, Bucket) ->
+    ((N + 10) - Bucket).
+
+print_stem(0) ->
+    io:format("~5s|", ["-0"]);
+print_stem(N) when N < 0 ->
+    io:format("~5B|", [N div 10]);
+print_stem(N) ->
+    io:format("~5B|", [N div 10 - 1]).
+
+plot_leaf([H|T], Bucket) when is_integer(H) andalso H < Bucket -> 
+    io:format("~w", [leaf(H, Bucket)]), plot_leaf(T, Bucket);
+plot_leaf([H|T], Bucket) when is_integer(H) -> 
+    io:format("~n"), plot_stem([H|T], Bucket+10);
+plot_leaf([], _Bucket) -> 
+    io:format("~n").
+
+plot_stem(Data, Bucket) -> 
+    print_stem(Bucket),
+    plot_leaf(Data, Bucket).
+
+plot_sorted([H | T]) -> plot_stem([H | T], bucket(H)).
+
+plot([]) -> io:format("~n");
+plot(Data) when is_list(Data) -> plot_sorted(lists:sort(Data)).
